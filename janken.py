4ce6221234c0f40972746ef0a7df193f6b776c78
moritarui <r.morita.sys24@morijyobi.ac.jp>
2024-11-08 13:55:23 +0900

    じゃんけんゲームの作成

--- janken.py
+++ janken.py
@@ -11,16 +11,17 @@ pc1 = ["グー", "チョキ", "パー"]
 
 
 # 勝敗数
+
 print(f'あなた：{cnt2}勝')
 print(f'コンピューター：{cnt1}勝')
 
 
 # 勝敗を判定
-
 if cnt2 > cnt1:
     print('あなたの総合勝利です！')
 elif cnt2 < cnt1:
     print('コンピューターの総合勝利です！')
 else:
     print('引き分けです！')
+
     
