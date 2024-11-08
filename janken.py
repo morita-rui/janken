6b31e8d4a30d24b5bed1dda245300db02a4c6e75
mukaiguti <y.mukaiguchi.sys24@morijyobi.ac.jp>
2024-11-08 13:19:08 +0900

    じゃんけんゲームの作成#4

--- janken.py
+++ janken.py
@@ -5,10 +5,12 @@ cnt2 = 0
 pc1 = ["グー", "チョキ", "パー"]
 
 # ユーザーの入力
+for ken in range(1, 4):
+    while True:  
+        pc_jan = random.choice(pc1)  
+        user_jan = int(input(f'----- ラウンド {ken} -----\n1.グー 2.チョキ 3.パー\nあなたの手を選択してください。>')) - 1
 
-
-
-
+s
 
 # 勝敗数
 
